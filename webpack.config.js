d5d1b19ba100a2c291dce0b24ef15744219c35ae
pengmengqiu <pengmengqiu@bytedance.com>
2019-05-07 11:18:02 +0800

    [mengqiu] use chunkhash for output file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
-var context = path.resolve(__dirname);
-console.log(context);
 module.exports = {
-	context: context,
 	entry: {
 		index: './src/index.js',
 		main: './src/main.js'
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[hash].js',
+		filename: '[name].[chunkhash].js',
 	}
 }
